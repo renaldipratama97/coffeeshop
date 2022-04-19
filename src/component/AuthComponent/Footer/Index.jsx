55e5d865768b81e810e2d3c44410698d35324181
ade <ade.saputra@vadhana.co.id>
2022-04-19 16:00:49 +0700

    create layout footer

--- src/component/AuthComponent/Footer/Index.jsx
+++ src/component/AuthComponent/Footer/Index.jsx
@@ -3,8 +3,17 @@ import React from "react";
 export default function Index() {
   return (
     <>
-      <div className="flex justify-center items-center bg-blue-600 w-100 h-[350px] bg-gray">
-        <p className="text-4xl text-white">this component is under development </p>
+      <div className="flex w-50 h-[350px] bg-[#F8F8F8]">
+        <div className="w-8/12 pt-[80px] px-[70px]">
+          <div className="flex items-center gap-3">
+            <div className="w-[25px] h-[28px]">
+              <img src={require("../../../assets/img/logo.png")} alt="logo" />
+            </div>
+            <p className="text-blacked font-bold text-[16px]">Coffee Shop</p>
+          </div>
+          <p className="leading-6 mt-5 font-bold text-[#4F5665] text-[15px]">Coffee Shop is a store that sells some good meals, and especially coffee. We provide high quality beans</p>
+        </div>
+        <div className="border w-6/12"> test2 </div>
       </div>
     </>
   );
