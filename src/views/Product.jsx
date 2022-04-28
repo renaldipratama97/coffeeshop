76926f7630d2b871cfb374a28e96bb44ac35fbd7
adesaputra7 <adesaputrakom@gmail.com>
2022-04-28 19:23:07 +0700

    design sidebar

--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -13,7 +13,14 @@ export default function index() {
                 <p className="text-[12px] w-[230px] text-center mt-10 ml-auto mr-auto">Coupons will be updated every weeks. Check them out! </p>
 
                 <div className="card mt-12 bg-[#FFCB65] h-[472px] w-[284px] ml-auto mr-auto rounded-[25px]">
-                    <img src={require("../assets/img/image-29.png")} className="rounded-full ml-auto mr-auto" alt="spaghetti" />
+                    <span className="img-card mt-10">
+                        <img src={require("../assets/img/image-29.png")} className="rounded-full ml-auto mr-auto" alt="spaghetti" />
+                    </span>
+                    <h2 className="font-bold text-center mt-3">Beef Spaghetti</h2>
+                    <h2 className="font-bold text-center mt-1">20% OFF</h2>
+                    <p className="text-center text-[12px] w-[80%] ml-auto mr-auto mt-2">
+                        Buy 1 Choco Oreo and get 20% off for Beef Spaghetti
+                    </p>
                 </div>
             </div>
             <div className="content w-[70%]">
